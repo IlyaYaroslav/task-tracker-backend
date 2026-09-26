9c9092aad154b0be47af45da6a755d59a7d6f1db
Yaroslav <lordrom92@gmail.com>
2026-09-26 20:04:04 +0300

    feat: modify tasks table schema to enforce constraints and improve indexing

--- src/main/resources/db.changelog/sql/003-create-tasks.sql
+++ src/main/resources/db.changelog/sql/003-create-tasks.sql
@@ -1,12 +1,12 @@
-CREATE TABLE tasks
+CREATE TABLE IF NOT EXISTS tasks
 (
     id          UUID PRIMARY KEY,
-    title       VARCHAR(200),
-    description VARCHAR(800),
-    state       VARCHAR(25),
-    project_id  UUID REFERENCES projects(id) CASCADE DELETE
-    createdAt   TIMESTAMP,
-    updatedAt   TIMESTAMP
+    title       VARCHAR(100) NOT NULL,
+    description VARCHAR(2000) NOT NULL,
+    state       VARCHAR(25) NOT NULL,
+    project_id  UUID NOT NULL REFERENCES projects(id),
+    created_at  TIMESTAMP NOT NULL,
+    updated_at  TIMESTAMP NOT NULL
 );
 
-CREATE INDEX IX_TASKS ON tasks (state);
+CREATE INDEX IX_TASKS ON tasks (state, project_id);
